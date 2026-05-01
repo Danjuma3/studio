c9820190d4b25cf3b804999fd70ed22ab0848c2a
Jalapeno smokers <djummeryahaya@gmail.com>
2026-05-01 01:40:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9268916124-3a0c3",
+  "appId": "1:506436269003:web:2b669087f9c4f61ea23c4e",
+  "apiKey": "AIzaSyD_FldnVbT8g0w0hapyoJcH9kSs8_k5cN4",
+  "authDomain": "studio-9268916124-3a0c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "506436269003"
+};
